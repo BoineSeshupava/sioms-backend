52bbe3908dc8202926d6d7e110e371e9ff27b251
BoineSeshupava <seshupavanyadav@gmail.com>
2025-04-06 19:30:05 +0530

    Initial Projects Setup Commit

--- SIOMS.API/Program.cs
+++
@@ -1,5 +0,0 @@
-// Entry Point for ASP.NET Core Web API
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-app.MapGet("/", () => "SIOMS Backend Running...");
-app.Run();
